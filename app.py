3fdbdaa99dfb0b92c4414be89f5f967a873e5e83
Ben Christians <bbc7909@rit.edu>
2019-02-17 08:32:00 -0500

    remove more gcp stuff

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ app = Flask(__name__)
 app.debug = False
 
 ghetto_database = 'static/active.txt'
-local = False
 
 
 @app.route('/ping')
@@ -14,23 +13,20 @@ def ping():
 
 @app.route('/set_active/<stream_name>')
 def set_active(stream_name):
-    if local:
-        open(ghetto_database, 'w').write(stream_name)
+    open(ghetto_database, 'w').write(stream_name)
 
     return "Made " + stream_name + " active"
 
 
 @app.route('/set_all_streams_inactive')
 def set_all_streams_inactive():
-    if local:
-        open(ghetto_database, 'w').write("")
+    open(ghetto_database, 'w').write("")
     return "Stopped all streams"
 
 
 @app.route('/is_active/<stream_name>')
 def is_active(stream_name):
-    if local:
-        active_stream = open(ghetto_database, 'r').read()
+    active_stream = open(ghetto_database, 'r').read()
     return jsonify({
         'is_active': stream_name == active_stream
     })
